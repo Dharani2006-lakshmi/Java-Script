0aa33dff25f0871081aaaf582bf304324fbafbc6
Dharani2006-lakshmi <d52410418@gmail.com>
2025-07-20 18:53:46 +0530

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,37 @@
+let a = 10; b = 3;
+console.log(a + b);        
+console.log(a - b);   
+console.log(a * b);   
+console.log(a / b);     
+console.log(a % b);    
+
+let x = 5;
+x += 3; 
+console.log(x);  
+x -= 2;
+console.log(x); 
+
+let m = 10;
+let n = "10";
+console.log(m == n);   
+console.log(m === n);  
+console.log(m != n);   
+console.log(m !== n); 
+console.log(m > 5);   
+console.log(m < 5);
+console.log(m <= 10);  
+console.log(m >= 5);
+
+let p = true;
+let q = false;
+console.log(p && q);  
+console.log(p || q);  
+console.log(!p);     
+
+let count = 1;
+console.log(count);    
+count++;
+console.log(count);    
+
+count--;
+console.log(count);    
